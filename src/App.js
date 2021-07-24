d44620f5bbd9d0c8b898bd5040790dea960126ea
crraay <crraay@gmail.com>
2021-07-24 11:08:19 +0700

    create some simple components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+const StaticTextComponent = () => {
+  return <div>Static text</div>
+}
+
+const DynamicTextComponent = ({text = 'Default text'}) => {
+  return <div>{text}</div>
+}
+
+class SecondDynamicTextComponent extends React.Component {
+  render() {
+    return <div>{this.props.text || 'Default text'}</div>
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <StaticTextComponent></StaticTextComponent>
+
+        <DynamicTextComponent text="Dynamic text"></DynamicTextComponent>
+        <DynamicTextComponent></DynamicTextComponent>
+
+        <SecondDynamicTextComponent text="More dynamic text"></SecondDynamicTextComponent>
+        <SecondDynamicTextComponent></SecondDynamicTextComponent>
       </header>
     </div>
   );
